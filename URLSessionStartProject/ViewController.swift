9bade41964bc81fdd3e478435ea01ac09d819f4e
Oleg <olegkirsanov22@gmail.com>
2022-02-06 23:22:00 +0300

    Add model for parsing.

--- URLSessionStartProject/ViewController.swift
+++ URLSessionStartProject/ViewController.swift
@@ -9,9 +9,9 @@ import UIKit
 import CryptoKit
 
 class ViewController: UIViewController {
-
+    
     private let endpointClient = EndpointClient(applicationSettings: ApplicationSettingsService())
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,14 +20,13 @@ class ViewController: UIViewController {
     
     func executeCall() {
         let endpoint = GetNameEndpoint()
-        let completion: EndpointClient.ObjectEndpointCompletion<String> = { result, response in
+        let completion: EndpointClient.ObjectEndpointCompletion<Cards> = { result, response in
             guard let responseUnwrapped = response else { return }
-
+            
             print("\n\n response = \(responseUnwrapped.allHeaderFields) ;\n \(responseUnwrapped.statusCode) \n")
             switch result {
-            case .success(let team):
-                print("team = \(team)")
-                
+            case .success(let parsedData):
+                parsedData.printData()
             case .failure(let error):
                 print(error)
             }
@@ -36,40 +35,27 @@ class ViewController: UIViewController {
     }
 }
 
-final class GetNameEndpoint: ObjectResponseEndpoint<String> {
+final class GetNameEndpoint: ObjectResponseEndpoint<Cards> {
     
     override var method: RESTClient.RequestType { return .get }
-    override var path: String { "/v1/public/characters" }
-    let timeStamp = NSDate().timeIntervalSince1970
-    let publicApiKey = "0974ca1c2fd4f246e6c0f6a8816c072a"
-    let privateApiKey = "4fbd8a1701e52eba2fa8205ff864818148047132"
-    var obligatoryItems: [URLQueryItem] {
-        return [URLQueryItem(name: "ts", value: "\(timeStamp)"),
-        URLQueryItem(name: "apikey", value: "\(publicApiKey)"),
-        URLQueryItem(name: "hash", value: "\(md5Hash(String(timeStamp) + privateApiKey + publicApiKey))")]
-    }
+    override var path: String { "/v1/cards" }
     
     override init() {
         super.init()
-        queryItems = [URLQueryItem(name: "name", value: "Groot")] + obligatoryItems
+        queryItems = [URLQueryItem(name: "name", value: "Overtaker|Black Lotus")]
     }
-    
-    private func md5Hash(_ source: String) -> String {
-        return Insecure.MD5.hash(data: source.data(using: .utf8)!).map { String(format: "%02hhx", $0) }.joined()
-    }
-
 }
 
 func decodeJSONOld() {
     let str = """
-        {\"team\": [\"ios\", \"android\", \"backend\"]}
+        {\"cards\": [\"ios\", \"android\", \"backend\"]}
     """
     
     let data = Data(str.utf8)
-
+    
     do {
         if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
-            if let names = json["team"] as? [String] {
+            if let names = json["cards"] as? [String] {
                 print(names)
             }
         }
